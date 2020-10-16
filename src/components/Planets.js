20fa69284b34922e7bd413fda3542bce517d3dd1
Ahmed Kamel <48414474+AhmedKamel95@users.noreply.github.com>
2020-10-16 15:40:58 +0200

    Finished app

--- src/components/Planets.js
+++ src/components/Planets.js
@@ -1,9 +1,53 @@
 import React from "react";
+import Planet from "./Planet";
+import { usePaginatedQuery } from "react-query";
+
+const fetchPlanets = async (key, page) => {
+  const res = await fetch(`http://swapi.dev/api/planets/?page=${page}`);
+  return res.json();
+};
 
 const Planets = () => {
+  const [page, setPage] = React.useState(1);
+  const { resolvedData, latestData, status } = usePaginatedQuery(
+    ["planets", page],
+    fetchPlanets
+  );
+
   return (
     <div>
       <h2>Planets</h2>
+
+      {status === "loading" && <div>Loading data</div>}
+
+      {status === "error" && <div>Error fetching data</div>}
+
+      {status === "success" && (
+        <>
+          <button
+            onClick={() => setPage((old) => Math.max(old - 1, 1))}
+            disabled={page === 1}
+          >
+            Previous page
+          </button>
+          <span>{page}</span>
+          <button
+            onClick={() =>
+              setPage((old) =>
+                !latestData || !latestData.next ? old : old + 1
+              )
+            }
+            disabled={!latestData || !latestData.next}
+          >
+            Next page
+          </button>
+          <div>
+            {resolvedData.results.map((planet) => (
+              <Planet key={planet.name} planet={planet} />
+            ))}
+          </div>
+        </>
+      )}
     </div>
   );
 };
